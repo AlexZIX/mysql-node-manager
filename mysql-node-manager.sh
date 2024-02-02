22f676eed23e9eae22b33efafda8630f9b9b519e
AlexZIX <alpletnyov@yandex.ru>
2024-02-02 23:22:27 +0300

    Check if MySQL node running before checking super_read_only

--- mysql-node-manager.sh
+++ mysql-node-manager.sh
@@ -2,21 +2,23 @@
 
 TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
-# Read how many nodes unreachable
+MYSQL_AVAILABLE=$((`mysql -Bse "SELECT 1;"`))
+
+# Read super read only
 SUPER_READ_ONLY=$((`mysql -Bse "SELECT @@super_read_only;"`))
 
 # Check if rule already exists
 sudo iptables -C INPUT -p tcp --dport 3306 -j DROP
 RULE_NOT_EXISTS=$?
 
-if [ $SUPER_READ_ONLY -eq 1 ] 
+if [ $SUPER_READ_ONLY -eq 1 ] || [ $MYSQL_AVAILABLE -eq 0 ]
 then	
 	if [ $RULE_NOT_EXISTS -eq 1 ]
 	then
 		# Add if not
 		sudo iptables -A INPUT -p tcp --dport 3306 -j DROP
 		
-		echo "[$TIMESTAMP] This node lost cluster connectivity. Add lock rule to iptables." >> /var/log/mysql-node-manager/mysql-node-manager.log
+		echo "[$TIMESTAMP] This node lost cluster connectivity or goes down. Add lock rule to iptables." >> /var/log/mysql-node-manager/mysql-node-manager.log
 	fi
 else
 	if [ $RULE_NOT_EXISTS -eq 0 ]
